3c2404b186a63e725dc1bae6910fb59a592d05ef
Albert Liu <albert.liu@frequentz.com>
2017-11-28 09:40:30 -0800

    Logging

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ function main() {
     let connection = db.getConnection(config.DATABASE_URL);
     let runTimestamp = Date.now();
     let test = !isNaN(process.argv[2]) && process.argv[2];
+    if(test) {
+        console.log('test mode!');
+        console.log('hook url: ', config.TESTHOOK_URL);
+        console.log('timestamp: ', test);
+    }
     db.getChanges(connection, test || config.LAST_RUN)
     .then(changes => { return discord.postChanges(test ? config.TESTHOOK_URL : config.WEBHOOK_URL, config.OP_URL, changes); })
     .then(() => { if(!test) { conf.setTimestamp(runTimestamp); } })
